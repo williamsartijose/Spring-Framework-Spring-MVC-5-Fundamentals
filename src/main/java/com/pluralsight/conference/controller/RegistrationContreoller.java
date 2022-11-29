6c78ff19477e28e02d6f9e9dc6e76a0c3a207233
unknown <williamsartijose@hotmail.com>
2022-11-29 08:33:46 +0000

    Passing Parameters

--- src/main/java/com/pluralsight/conference/controller/RegistrationContreoller.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationContreoller.java
@@ -1,7 +1,10 @@
 package com.pluralsight.conference.controller;
 
+import com.pluralsight.conference.modal.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
@@ -9,7 +12,13 @@ import java.util.Map;
 public class RegistrationContreoller {
 
     @GetMapping("registration")
-    public String getRegistration(Map<String, Object> model) {
+    public String getRegistration(@ModelAttribute ("registration")Registration registration) {
         return "registration";
+    }
+
+    @PostMapping("registration")
+    public String addRegistration(@ModelAttribute ("registration")Registration registration) {
+        System.out.println("Registration: " + registration.getName());
+        return  "registration";
     }
 }
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -6,11 +6,29 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="from" uri="http://www.springframework.org/tags/form" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
 <h1>Hello Registration</h1>
+<from:form modelAttribute="registration">
+    <table>
+        <tr>
+            <td>
+                Name:
+            </td>
+            <td>
+                <from:input path="name"/>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <input type="submit" value="Add Registration">
+            </td>
+        </tr>
+    </table>
+</from:form>
 </body>
 </html>
